c7696714234fd7fc9f22e68bb2ca612e560f4fc7
jyvgc <kjyg2627@gmail.com>
2026-04-16 20:31:16 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,30 @@
-self.addEventListener('install',function(){self.skipWaiting();});
-self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.map(function(n){return caches.delete(n);}));}));});
+var CACHE = 'dept-cal-v2';
+var FILES = [
+  '/dept-calendar/',
+  '/dept-calendar/index.html',
+  '/dept-calendar/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(FILES); })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).catch(function() { return caches.match('/dept-calendar/index.html'); });
+    })
+  );
+});
